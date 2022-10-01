568f5049a12608a128ecd6d296be053d594d281f
Royer Hernández <ing.royerhernandez@gmail.com>
2022-10-01 17:02:35 -0500

    Operadores arimeticos

--- main.go
+++ main.go
@@ -2,38 +2,79 @@ package main
 
 import "fmt"
 
-func main() {
-	// Declaración de Constantes
-
-	const pi float64 = 3.1415;
-	const pi2 = 3.141516
-	
-	fmt.Println("pi1:", pi)
-	fmt.Println("pi2:", pi2)
-
-	// Declaración de Variables
-
-	base:= 12
-	var altura int = 14
-	var area int
-
-	fmt.Println(base, altura, area)
-
-	// Zero Values
-
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println(a,b,c,d)
-
+func main() {	
 	// Area de un Cuadrado
 
 	const baseCuadrado int = 10
     areaCuadrado:= baseCuadrado * baseCuadrado
 
 	fmt.Println("Área Cuadraro:", areaCuadrado)
+
+	// Suma
+
+	x:= 10
+	y:= 20
+	result:= x+y
+
+	fmt.Println("Suma:", result)
+
+	// Resta
+
+	result =  y - x
+	fmt.Println("Resta:", result)
+
+	// Multiplicacion
+
+	result = x * y
+	fmt.Println("Multiplicación:", result)
+
+	// División
+
+	result = y/x
+	fmt.Println("División:", result)
+
+	//Modulo
+
+	result = y % x
+	fmt.Println("Modulo:", result)
+
+	// Incremental
+
+	x++
+	fmt.Println("Incremental:", x)
+
+	// Decremental
+
+	x--
+	fmt.Println("Decremental:", x)
+
+	// Retos
+
+	// Rectangulo base * altura
+
+	baseRectangulo:= 10
+	alturaRectangulo:= 40
+	areaRectangulo:= baseRectangulo * alturaRectangulo
+
+	fmt.Println("Área Rectangulo:", areaRectangulo)
+
+	// Trapecio (Base + base) * altura / 2
 	
-	fmt.Println("Hello World")
+	base:= 10
+	Base:= 40
+	altura:= 30
+
+	areaTrapecio:= ((base+Base) * altura) / 2
+
+	fmt.Println("Área Trapecio:", areaTrapecio)
+
+	// Circulo pi * radio 2
+
+	var radio float64= 6
+	const pi float64 = 3.14159
+
+	areaCirculo:= pi * (radio*radio)
+
+	fmt.Println("Área Circulo:", areaCirculo)
+
 }
