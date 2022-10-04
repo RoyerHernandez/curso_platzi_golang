0f409a0a48746a932fac19f86b07b84378b3baf7
Royer Hernández <ing.royerhernandez@gmail.com>
2022-10-03 21:31:10 -0500

    Arrays y Slices

--- main.go
+++ main.go
@@ -4,24 +4,35 @@ import "fmt"
 
 func main() {
 
-	// Defer
+	// Array
+	var array[4]int
+	fmt.Println(array)
 
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	array[0] = 1
+	array[1] = 2
 
-	// Continue y Break
+	fmt.Println(array, len(array), cap(array))
 
-	for i := 0 ; i < 10; i++{
-		fmt.Println(i)
-		if(i == 2){
-			fmt.Println("Es 2")
-			continue
-		}
+	// Slice
 
-		if(i ==8){
-			fmt.Println("Es 8")
-			break
-		}
-	}
-	
+	slice:= [] int{1,2,3,4,5,6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// Metodos en el Slice
+
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append Slice
+
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	//  new Slice
+	newSlice:= []int{8,9,10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
+
 }
