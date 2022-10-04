4785660ec466085394e7dcef10eb4fe195e8f9db
Royer Hernández <ing.royerhernandez@gmail.com>
2022-10-03 21:47:03 -0500

    Recorrido de Slices con Range

--- main.go
+++ main.go
@@ -2,37 +2,30 @@ package main
 
 import "fmt"
 
+func isPalindromo(texto string){
+
+	textReverse:= ""
+
+	for i:= len(texto) - 1; i >=0; i--{
+		textReverse += string(texto[i])
+	}
+
+	if(textReverse == texto){
+		fmt.Println("Es un Palindromo")
+	}else{
+		fmt.Println("No es un Palindromo")
+	}
+
+}
+
 func main() {
 
-	// Array
-	var array[4]int
-	fmt.Println(array)
+	isPalindromo("amor al roma")
 
-	array[0] = 1
-	array[1] = 2
+	slice:= []string{"Hola", "como", "estas"}
 
-	fmt.Println(array, len(array), cap(array))
-
-	// Slice
-
-	slice:= [] int{1,2,3,4,5,6}
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Metodos en el Slice
-
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	// Append Slice
-
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	//  new Slice
-	newSlice:= []int{8,9,10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	for i, valor := range slice{
+		fmt.Println(i,valor)
+	}
 
 }
