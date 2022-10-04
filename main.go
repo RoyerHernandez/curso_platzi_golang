4d87c02abe9b69ae394af07896c0c06c2398103e
Royer Hernández <ing.royerhernandez@gmail.com>
2022-10-03 20:31:27 -0500

    Multiples condiciones anidades con Switch

--- main.go
+++ main.go
@@ -1,92 +1,29 @@
 package main
 
-import (
-	//"fmt"
-	"fmt"
-	"log"
-	"strconv"
-)
+import "fmt"
 
-func esPar(par string) int{
-	value, error := strconv.Atoi(par)
-	result  := 0
-	if error!= nil{
-		log.Fatal(error)
-	}else{
-		module := value % 2
-		if module!=0{
-		result = 1
-		}else{
-		result = 2
-		}		
-	} 
-	return  result
-}
+func main() {
 
-func validaPassword(user string, pass int) int{
-	usuario:= user
-	contraseña:= pass
-	respuesta := 0
-
-	if(usuario == "Israfel2112" && contraseña == 123){
-		respuesta = 1
-	}
-	return respuesta
-}
-
-
-func main() {	
-	//Condicionales
-	Valor1:= 1
-	Valor2:= 2
-
-	if(Valor1 == Valor2){
-		fmt.Println("Son iguales")
-	}else{
-		fmt.Println("Son diferentes")
-	}
-
-	// Puerta logica and &&
+	// Switch
 	
-	if Valor1 == 1 && Valor2 ==3{
-		fmt.Println("Es correcto")
-	}else{
-		fmt.Println("Es incorrecto")
+
+	switch modulo := 4 % 2; modulo {
+	case 0:
+		fmt.Println("es par")
+	default:
+		fmt.Println("es impar")
 	}
 
-	// Puerta logica or ||
-	
-	if Valor1 == 1 || Valor2 ==3{
-		fmt.Println("Es correcto")
-	}else{
-		fmt.Println("Es incorrecto")
+	// Sin Condicion
+
+	value:= 300
+
+	switch {
+	case value > 200:
+		fmt.Println("Es mayor a 200")
+	case value < 0:
+		fmt.Println("ES menor a cero") 
+	default:
+		fmt.Println("No hay condicion")
 	}
-
-	// Convertir texto a número
-
-	valor, error:= strconv.Atoi("89")
-
-	if error != nil{
-		log.Fatal(error)
-	}else{
-		fmt.Println("Value: ", valor)
-	}
-
-	esPar:= esPar("2")
-
-	if esPar  == 1{
-		fmt.Println("El número ingresado no es par", )
-	}else{
-		fmt.Println("El número ingresado es par")
-	}
-	
-	validaPassword:= validaPassword("Israfel2112", 123)
-
-	if validaPassword != 1{
-		fmt.Println("El usuario no es valido")
-	}else{
-		fmt.Println("El usuario es valido")
-	}
-	// 
-
 }
