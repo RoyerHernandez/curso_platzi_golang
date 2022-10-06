d0b0e3f37286427e21a260330a26c611a684812a
Royer Hernández <ing.royerhernandez@gmail.com>
2022-10-05 21:24:59 -0500

    Structs y Punteros

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
-import (
-	pk "curso_platzi_golang/src/mypackage"
-	"fmt"
-)
+import "fmt"
+
+type pc struct{
+	ram int
+	disk int
+	brand string
+}
+
+func (myPc pc) ping(){
+	fmt.Println(myPc.brand, "pong")
+}
+
+func (myPc *pc) duplicateRam(){
+	myPc.ram = myPc.ram * 2
+}
 
 func main() {
 
-	var myCar = pk.CarPublic{}
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2022
-	fmt.Println(myCar)
+	a:=50
+	b:=&a
 
-	pk.PrintMessage("Hola Mundo")
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	*b=100
+	fmt.Println(a)
+
+	myPc:= pc{ram: 16, disk: 500, brand:"MSI"}
+
+	fmt.Println(myPc)
+	myPc.ping()
+
+	fmt.Println(myPc)
+	myPc.duplicateRam()
+
+	fmt.Println(myPc)
+	myPc.duplicateRam()
+
+	fmt.Println(myPc)
+	myPc.duplicateRam()
 
 }
